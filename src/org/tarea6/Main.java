80bd1d03248eee6e09f0ab6db7798363f8e5a7e7
Ye117 <q502461275@gmail.com>
2026-06-14 02:04:43 +0200

    Primer commit

---
+++ src/org/tarea6/Main.java
@@ -0,0 +1,27 @@
+package org.tarea6;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author q5024
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+    miCarrito.aplicarDescuento(NUM_A, NUM_B);
+    }
+    private static final int NUM_B = 5;
+    private static final int NUM_A = 100;
+   
+    
+}
